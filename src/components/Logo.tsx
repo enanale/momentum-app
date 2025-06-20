5a9aededcd69ded9efdcf03e3cf8657e463d3ce1
Ed Nanale <enanale@gmail.com>
2025-06-19 17:51:34 -0700

    feat(branding): Replace app icon with fast-forward 'M' design (v13)

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -10,7 +10,7 @@ export const Logo = ({ height = 32, sx }: LogoProps) => {
   return (
     <Box
       component="img"
-      src="/logo-v7.svg"
+      src="/logo-v13.svg"
       alt="Momentum Logo"
       sx={{
         height,
